b53072e681ea64389881eb0ed21b8d9a9e513893
Denis Koco <deniskoco89@gmail.com>
2025-02-23 20:46:45 +0100

    fix: Pass correct quantity to cart and save to cookies

--- app/products/[id]/Productitem.js
+++ app/products/[id]/Productitem.js
@@ -67,19 +67,8 @@ export default function ProductItem(props) {
         />
 
         <button
-          onClick={() => {
-            const cart = JSON.parse(localStorage.getItem('cart')) || [];
-            const existingProduct = cart.find(
-              (item) => item.id === props.productId,
-            );
-
-            if (existingProduct) {
-              existingProduct.quantity = quantity;
-            } else {
-              cart.push({ ...product, quantity });
-            }
-
-            localStorage.setItem('cart', JSON.stringify(cart));
+          onClick={async () => {
+            await addOrUpdateCartItem(props.productId, quantity);
             alert(`${product.name} added to cart!`);
             router.push('/cart');
           }}
