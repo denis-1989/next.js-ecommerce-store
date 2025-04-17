261eeab74629ffb1927cb536e7739da301dc7481
Denis Koco <deniskoco89@gmail.com>
2025-04-17 20:44:41 +0200

    Add new changes in fly-io.sh

--- scripts/fly-io-start.sh
+++ scripts/fly-io-start.sh
@@ -3,16 +3,13 @@
 # Exit if any command exits with a non-zero exit code
 set -o errexit
 
-
-if [[ ! -f /postgres-volume/data/postgresql.conf ]]; then
+if [[ ! -f /postgres-volume/run/postgresql/data/postgresql.conf ]]; then
   echo "❗️ No PostgreSQL database found, run the setup script"
   sleep infinity
 fi
 
 echo "Setting up PostgreSQL on Fly.io..."
-
-
-su postgres -c "pg_ctl start --pgdata=/postgres-volume/data"
+su postgres -c "pg_ctl start --pgdata=/postgres-volume/run/postgresql/data"
 
 pnpm migrate up
-pnpm start
+./node_modules/.bin/next start
