555f56726b92488c047cf2066e321d5592d7be10
Denis Koco <deniskoco89@gmail.com>
2025-04-17 20:29:06 +0200

    fix: change postgres data dir to /postgres-volume/data to avoid permission issue

--- scripts/fly-io-start.sh
+++ scripts/fly-io-start.sh
@@ -3,13 +3,16 @@
 # Exit if any command exits with a non-zero exit code
 set -o errexit
 
-if [[ ! -f /postgres-volume/run/postgresql/data/postgresql.conf ]]; then
+
+if [[ ! -f /postgres-volume/data/postgresql.conf ]]; then
   echo "❗️ No PostgreSQL database found, run the setup script"
   sleep infinity
 fi
 
 echo "Setting up PostgreSQL on Fly.io..."
-su postgres -c "pg_ctl start --pgdata=/postgres-volume/run/postgresql/data"
+
+
+su postgres -c "pg_ctl start --pgdata=/postgres-volume/data"
 
 pnpm migrate up
-./node_modules/.bin/next start
+pnpm start
